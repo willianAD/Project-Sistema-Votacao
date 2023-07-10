fcf7ddd7a5bb92e583e5e55b32ecbda26ec12090
willianAD <willian.afonso013@gmail.com>
2023-07-10 15:27:57 -0300

    feat: class Pessoa - Requisito 1

--- src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
+++ src/main/java/com/betrybe/sistemadevotacao/Pessoa.java
@@ -1,5 +1,13 @@
 package com.betrybe.sistemadevotacao;
 
 public abstract class Pessoa {
+  protected String nome;
 
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
