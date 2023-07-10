b2f32ac2d999eaf22a41a583ed10d21e549948fc
willianAD <willian.afonso013@gmail.com>
2023-07-10 15:55:31 -0300

    feat: class PessoaCandidata - Requisito 1

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,32 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public abstract class PessoaCandidata extends Pessoa {
+  public int numero;
+  public int votos;
 
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
+
+  public int getNumero() {
+    return numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
 }
