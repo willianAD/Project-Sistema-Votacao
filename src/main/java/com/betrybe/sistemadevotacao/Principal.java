3192d7e3e19d07bbd999aea58fddcbc1da3c0b09
willianAD <willian.afonso013@gmail.com>
2023-07-11 13:08:25 -0300

    feat: class Principal - Requisito 7

--- src/main/java/com/betrybe/sistemadevotacao/Principal.java
+++ src/main/java/com/betrybe/sistemadevotacao/Principal.java
@@ -1,10 +1,86 @@
 package com.betrybe.sistemadevotacao;
 
+import java.util.Scanner;
+
 public class Principal {
 
+  /** Not a javadoc (ignored). */
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
-  }
+    GerenciamentoVotacao gerenciamento = new GerenciamentoVotacao();
+    System.out.println("Selecione uma etapa:");
+    System.out.println("1 - Cadastro de pessoas candidata");
+    System.out.println("2 - Cadastro de pessoas eleitoras");
+    System.out.println("2 - Votação");
 
+    Scanner scanner = new Scanner(System.in);
+    short menuOption = scanner.nextShort();
+
+    switch (menuOption) {
+      case 1:
+        System.out.println("Cadastrar pessoa candidata?");
+        System.out.println("1 - Sim");
+        System.out.println("2 - Não");
+        System.out.println("Entre com o número correspondente à opção desejada:");
+        String select1 = scanner.nextLine();
+        if (select1 == "1 - Sim") {
+          System.out.println("Entre com o nome da pessoa candidata:");
+          String nomeInput = scanner.nextLine();
+          String nome = nomeInput;
+          System.out.println("Entre com o número da pessoa candidata:");
+          String numeroInput = scanner.nextLine();
+          int numero = Integer.parseInt(numeroInput);
+          gerenciamento.cadastrarPessoaCandidata(nome, numero);
+        } else {
+          System.exit(0);
+          scanner.close();
+        }
+        break;
+
+      case 2:
+        System.out.println("Cadastrar pessoa eleitora?");
+        System.out.println("1 - Sim");
+        System.out.println("2 - Não");
+        System.out.println("Entre com o número correspondente à opção desejada:");
+        String select2 = scanner.nextLine();
+        if (select2 == "1 - Sim") {
+          System.out.println("Entre com o nome da pessoa eleitora:");
+          String nomeInput = scanner.nextLine();
+          String nome = nomeInput;
+          System.out.println("Entre com o cpf da pessoa eleitora:");
+          String cpfInput = scanner.nextLine();
+          String cpf = cpfInput;
+          gerenciamento.cadastrarPessoaEleitora(nome, cpf);
+        } else {
+          System.exit(0);
+          scanner.close();
+        }
+        break;
+
+      case 3:
+        System.out.println("Entre com o número correspondente à opção desejada:");
+        System.out.println("1 - Votar");
+        System.out.println("2 - Resultado Parcial");
+        System.out.println("3 - Finalizar Votação");
+        String select3 = scanner.nextLine();
+        if (select3 == "1 - Votar") {
+          System.out.println("Entre com o cpf da pessoa eleitora:");
+          String cpfInput = scanner.nextLine();
+          String cpf = cpfInput;
+          System.out.println("Entre com o número da pessoa candidata:");
+          String numeroInput = scanner.nextLine();
+          int numero = Integer.parseInt(numeroInput);
+          gerenciamento.votar(cpf, numero);
+        } else if (select3 == "2 - Resultado Parcial") {
+          gerenciamento.mostrarResultado();
+        } else {
+          System.exit(0);
+          scanner.close();
+        }
+        break;
+      default:
+        System.out.println("Opção Inválida.");
+    }
+  }
 }
